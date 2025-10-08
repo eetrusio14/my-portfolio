3bcc871f4b5ffa55d749f4ac5b964426be7d3906
Emma E Trusio <158231927+eetrusio14@users.noreply.github.com>
2025-10-08 15:11:02 -0400

    Update to include hyperlinks for GH and LinkedIn profiles.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Emma Trusio</h1>
-      <h2>Computer Science @ Seton Hall University</h2>
+      <h2>Computer Science + Data Analytics @ Seton Hall University</h2>
       
       <section>
         <h3>Resume</h3>
@@ -18,7 +18,7 @@ function App() {
         <h3>Projects</h3>
         <u1>
         <li>
-            Pirate Battle Game –
+            Pirate Battle Game – 
             <a
               href="https://github.com/eetrusio14/pirate-game"
               target="_blank"
@@ -31,16 +31,33 @@ function App() {
 
       <section>
         <h3>Internship Experience</h3>
-        <p>Colgate-Palmolive - Incoming Fall 2025</p>
+        <p>Colgate-Palmolive Corp. - Fall 2025</p>
         <p>Pacira Biosciences - Summer 2025</p>
-        <ul>
-          <li>To be populated...</li>
-        </ul>
+        <p>Schindler Elevator Corp. - Summer 2024</p>
       </section>
 
       <section>
-        <h3>Contact Me -</h3>
-        <p>Email: eetrusio@gmail.com</p>
+        <h3>Contact Me</h3>
+        <u1>
+          <p>
+            Email: eetrusio@gmail.com
+          </p>
+          <li>
+            LinkedIn:
+            <a 
+              href="www.linkedin.com/in/emmatrusio"
+              target="_blank"
+              rel="noopener noreferrer">
+            </a>
+          </li>
+          <li>
+            <a
+            href="www.github.com/eetrusio14"
+            target="_blank"
+            rel="noopener nonreferrer">
+            </a>
+          </li>
+        </u1>
       </section>
     </div>
   );
